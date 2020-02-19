f3f01c3363b8b71b96c4d1ae9295cf5e1b27633b
SamplesMaker <insert@kakao.com>
2020-02-20 08:27:31 +0900

    SwiftUI 세그먼트 컨트롤

--- SegmentedControlSample/ContentView.swift
+++ SegmentedControlSample/ContentView.swift
@@ -9,8 +9,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selectorIndex = 0
+    @State private var numbers = ["One", "Two", "Three"]
+    
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            // Picker 를 이용하여 데이터 설정후 pickerStyle 로 세그먼트를 생성
+            Picker("Numbers", selection: $selectorIndex) {
+                ForEach(0 ..< numbers.count) { index in
+                    Text(self.numbers[index]).tag(index)
+                }
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            
+            Text("Selected Index Is \(numbers[selectorIndex])").padding()
+        }
     }
 }
 
